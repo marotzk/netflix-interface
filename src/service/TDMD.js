92c55de9c7a0aa683e2ae2514cca396ce7b32a24
marotzk <marotzki_wmbsk8@hotmail.com>
2021-10-01 16:36:38 -0300

    feat: add API service

---
+++ src/service/TDMD.js
@@ -0,0 +1,55 @@
+const API_KEY =  process.env.REACT_APP_API_KEY;
+const API_BASE = process.env.REACT_APP_API_BASE;
+
+const basicFecth = async (endpoint) => {
+  const req = await fetch(`${API_BASE}${endpoint}`);
+  const json = await req.json();
+  return json;
+}
+
+export const TMDB = {
+  getHomeList: async () => {
+    return [
+      {
+        slug: 'originals',
+        title: 'Originais Netflix',
+        items: await basicFecth(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'trending',
+        title: 'Recomendados para Você',
+        items: await basicFecth(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'toprated',
+        title: 'Em alta',
+        items: await basicFecth(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'action',
+        title: 'Ação',
+        items: await basicFecth(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'comedy',
+        title: 'Comédia',
+        items: await basicFecth(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'horror',
+        title: 'Terror',
+        items: await basicFecth(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'romance',
+        title: 'Romance',
+        items: await basicFecth(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'documentary',
+        title: 'Documentários',
+        items: await basicFecth(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+      },
+    ]
+  }
+}
